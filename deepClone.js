63ed7c64521800278f2d1772442cd84b5626a144
niusz <163yourba@163.com>
2023-05-18 22:49:36 +0800

    feat✨: 防抖函数

--- deepClone.js
+++ deepClone.js
@@ -1,1 +1,46 @@
-console.log('deepClone');
+/**
+ * 深拷贝
+ */
+const obj1 = {
+    age: 20,
+    name: 'xxx',
+    address: {
+        city: 'beijing'
+    },
+    arr: ['a', 'b', 'c']
+}
+
+const obj2 = deepClone(obj1)
+
+obj2.address.city = 'shanghai'
+console.log(obj1.address.city)
+
+/**
+ * 
+ * @param {Object} obj 要拷贝的对象
+ */
+function deepClone(obj = {}) {
+    if (typeof obj !== 'object' || obj == null) {
+        // obj 是null, 或者不是对象和数组，直接返回
+        return obj
+    }
+
+    // 初始化返回结果
+    let result
+    if (obj instanceof Array) {
+        result = []
+    } else {
+        result = {}
+    }
+
+    for (let key in obj) {
+        // 保证key不是原型的属性
+        if (obj.hasOwnProperty(key)) {
+            // 递归调用
+            result[key] = deepClone(obj[key])
+        }
+    }
+    // 返回结果
+    console.log('result', result);
+    return result
+}
